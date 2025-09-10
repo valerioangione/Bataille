f35539491920532dc92c1330622498122750b014
valerioangione <114509633+valerioangione@users.noreply.github.com>
2025-09-10 13:23:08 +0100

    Update Gaulois.java

--- src/Personnage/Gaulois.java
+++ src/Personnage/Gaulois.java
@@ -11,16 +11,15 @@ public class Gaulois extends Combattant{
 	
 //	public void parler(String texte) {
 //		StringBuilder txt = new StringBuilder();
-//		txt.append("Le gaulois ").append(this.getNom()).append(": «")
-//		.append(texte).append("».");
+//		txt.append("Le gaulois ").append(this.getNom()).append(": Â«")
+//		.append(texte).append("Â».");
 //		System.out.println(txt);
 //	}
 	
 	
 	public void boirePotion(int forcePotion) {
 		this.effetPotion=forcePotion;
-		this.parler("Merci Druide, je sens que ma force est " + effetPotion + " fois décuplée.");
-		System.out.println("aaaaaaa test");
+		this.parler("Merci Druide, je sens que ma force est " + effetPotion + " fois dÃ©cuplÃ©e.");
 	}
 	
 	@Override
@@ -34,7 +33,7 @@ public class Gaulois extends Combattant{
 	public void frapper(Combattant romain) {
 		StringBuilder txt = new StringBuilder();
 		txt.append(this.getNom())
-		.append(" envoie un grand coup dans la mâchoire de ").append(romain.getNom());
+		.append(" envoie un grand coup dans la mÃ¢choire de ").append(romain.getNom());
 		System.out.println(txt);
 		romain.recevoirCoup((this.getForce()/3)*this.effetPotion);
 		if (this.effetPotion>1) {this.effetPotion--;}
